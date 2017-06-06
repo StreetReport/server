8a0299fc550e731d18a83f5f3240abe18e0335dd
CEB-HEF42NJ\차은별 <ceb0527@ajou.ac.kr>
2017-06-06 19:49:41 +0900

    modify get/post

--- routes/report.js
+++ routes/report.js
@@ -6,41 +6,26 @@ var router = express.Router();
 var pool = require('../config/db')();
 
 //모든 report
-router.route('/')
-    .get(function (req, res, next) {
-        pool.getConnection(function (err, conn) {
-            var sqlForReportList = 'SELECT * FROM report ORDER BY id DESC';
-            conn.query(sqlForReportList, function (err, reports) {
-                if (err) {
-                    conn.release();
-                    throw err;
-                } else {
-                    res.json(reports);
-                    conn.release();
-                }
-            });
-        });
-    })
-    .post(function (req, res) {
-        var type = req.body.type;
-        var content = req.body.content;
-        var image = req.body.image;
-        var longitude = req.body.longitude;
-        var latitude = req.body.latitude;
+router.get('/:type/:userNickname/:content/:image/:longitude/:latitude', function (req, res, next) {
+    var type = parseInt(req.param.type);
+    var content = req.param.content;
+    var image = req.param.image;
+    var longitude = parseFloat(req.param.longitude);
+    var latitude = parseFloat(req.param.latitude);
 
-        pool.getConnection(function (err, conn) {
-            var sqlForPostReportCreate = 'INSERT INTO report(type, content, image, longitude, latitude) VALUES (?,?,?,?,?)';
-            conn.query(sqlForPostReportCreate, [type, content, image, longitude, latitude], function (err, row) {
-                if (err) {
-                    conn.release();
-                    throw err;
-                } else {
-                    res.json({"response": "ok"});
-                    conn.release();
-                }
-            });
-        })
-    })
+    pool.getConnection(function (err, conn) {
+        var sqlForCreateReport = 'INSERT INTO report(type, content, image, longitude, latitude) VALUES (?,?,?,?,?)';
+        conn.query(sqlForCreateReport, [type, content, image, longitude, latitude], function (err, row) {
+            if (err) {
+                conn.release();
+                throw err;
+            } else {
+                res.json({"response": "ok"});
+                conn.release();
+            }
+        });
+    });
+});
 
 //read
 router.get('/report/:id', function (req, res, next) {
