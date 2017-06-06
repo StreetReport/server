3083c7d18ef715457c41b5bf9b5549fdb242f92c
CEB-HEF42NJ\차은별 <ceb0527@ajou.ac.kr>
2017-06-07 01:59:47 +0900

    finally...complete..

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var pool = require('../config/db')();
 /* GET home page. */
 // router.get('/', function (req, res, next) {
 //     res.render('index', {title: 'Express'});
@@ -19,14 +19,13 @@ router.get('/:type', function (req, res, next) {
     var latitude = parseFloat(arr[3]);
     var longitude = parseFloat(arr[4]);
     var image = arr[5];
-    var reg_date = Date.now();
 
-    console.log(type, content, nickname, image, longitude, latitude);
 
     pool.getConnection(function (err, conn) {
-        var sqlForCreateReport = 'INSERT INTO report(type, nickname, content, latitude, longitude, image, reg_date) VALUES (?,?,?,?,?,?,?,?)';
-        conn.query(sqlForCreateReport, [type, nickname, content, latitude, longitude, image, reg_date], function (err, row) {
+        var sqlForCreateReport = 'INSERT INTO report(type, nickname, content, latitude, longitude, image, reg_date) VALUES (?, ?, ?, ?, ?, ?, NOW())';
+        conn.query(sqlForCreateReport, [type, nickname, content, latitude, longitude, image], function (err, row) {
             if (err) {
+                console.log(err);
                 conn.release();
                 throw err;
             } else {
