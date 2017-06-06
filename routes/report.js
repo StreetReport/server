ff393c876c9df358b32270462b829e5d81626c69
CEB-HEF42NJ\차은별 <ceb0527@ajou.ac.kr>
2017-06-07 01:40:01 +0900

    modify req.params

--- routes/report.js
+++ routes/report.js
@@ -3,16 +3,25 @@ var router = express.Router();
 var pool = require('../config/db')();
 
 //모든 report
-router.get('/:image/:type/:nickname/:content/:latitude/:longitude', function (req, res, next) {
-    var type = parseInt(req.params.type);
-    var content = req.params.content;
-    var nickname = req.params.nickname;
-    var image = req.params.image;
-    var longitude = parseFloat(req.params.longitude);
-    var latitude = parseFloat(req.params.latitude);
+router.get('/:type', function (req, res, next) {
+    var type = req.params.type;
+    var arr = type.split('&');
+    for(var i=0; i<arr.length; i++){
+        arr[i] = arr[i].split('=')[1];
+    }
+
+    var type = parseInt(arr[0]);
+    var nickname = arr[1];
+    var content = arr[2];
+    var latitude = parseFloat(arr[3]);
+    var longitude = parseFloat(arr[4]);
+    var image = arr[5];
+    var reg_date = Date.now();
+
+    console.log(type, content, nickname, image, longitude, latitude);
 
     pool.getConnection(function (err, conn) {
-        var sqlForCreateReport = 'INSERT INTO report(type, nickname, content, latitude, longitude, image, reg_date) VALUES (?,?,?,?,?,?,?,now())';
+        var sqlForCreateReport = 'INSERT INTO report(type, nickname, content, latitude, longitude, image, reg_date) VALUES (?,?,?,?,?,?,?,?)';
         conn.query(sqlForCreateReport, [type, nickname, content, latitude, longitude, image, reg_date], function (err, row) {
             if (err) {
                 conn.release();
